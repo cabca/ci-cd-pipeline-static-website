2758ece3e952d78a2de548bd6353cdc554e7c597
cabca <cabca@tuta.io>
2024-07-27 17:38:23 +0300

    updating some files

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,38 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_security_group" "web_sg" {
+  name        = "admin-dashboard-web-dev-sg"
+  description = "Allow SSH and HTTP traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_instance" "web" {
-  ami           = "ami-0427090fd1714168b" # Amazon Linux 2 AMI (Change as per your region)
+  ami           = "ami-0427090fd1714168b"
   instance_type = "t2.micro"
+  security_groups = [aws_security_group.web_sg.name]
 
   tags = {
-    Name = "StaticWebsiteServer"
+    Name = "MyWebServer"
   }
 }
