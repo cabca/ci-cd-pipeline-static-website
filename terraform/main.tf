6118fc6eeb154ce4e4918d189b6839d0ee691d84
cabca <cabca@tuta.io>
2024-07-27 17:48:49 +0300

    adsasdas

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,7 +9,7 @@ data "aws_security_group" "existing_sg" {
 resource "aws_instance" "web" {
   ami           = "ami-0427090fd1714168b"
   instance_type = "t2.micro"
-  security_groups = [aws_security_group.web_sg.name]
+  security_groups = [aws_security_group.existing_sg.name]
 
   tags = {
     Name = "MyWebServer"
