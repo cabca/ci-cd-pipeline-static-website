5dff204bf4da9b2a1448cf4fdfebef06feda46b9
cabca <cabca@tuta.io>
2024-07-27 17:45:13 +0300

    still updating

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,30 +2,8 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_security_group" "web_sg" {
-  name        = "admin-dashboard-web-dev-sg"
-  description = "Allow SSH and HTTP traffic"
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+data "aws_security_group" "existing_sg" {
+  id = "sg-0329c9d10fea2a8ca"
 }
 
 resource "aws_instance" "web" {
