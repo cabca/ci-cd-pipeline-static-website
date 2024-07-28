d66e11a2f4d769500ca70a8eda46fb754f9d6ad8
cabca <cabca@tuta.io>
2024-07-28 14:36:46 +0300

    might work

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,6 +10,7 @@ resource "aws_instance" "web" {
   ami           = "ami-0427090fd1714168b"
   instance_type = "t2.micro"
   vpc_security_group_ids = [data.aws_security_group.existing_sg.id]
+  key_name      = "GITHUB-ACTIONS-PIPELINE-KEY-PAIR"
 
   tags = {
     Name = "web"
